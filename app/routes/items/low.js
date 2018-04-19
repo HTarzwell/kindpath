ea7f5b1088eaf36e5d76f5e9de26cc8caa9d85ec
HTarzwell <htarzwell@gmail.com>
2018-04-19 13:28:43 -0400

    Added new font and modified

---
+++ app/routes/items/low.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route'
+
+export default Route.extend({
+  /* model () {
+    return this.get('store').findAll('item')
+  },
+  renderTemplate: function (item) {
+    item.get('store').filterBy('energy_needed', 'low')
+  } */
+})
